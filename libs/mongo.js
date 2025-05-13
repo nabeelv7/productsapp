74c7d1c3c90fc45b80cee107be8bc32dd043388d
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-13 17:17:46 +0500

    added comment to mongo.js

--- libs/mongo.js
+++ libs/mongo.js
@@ -5,6 +5,7 @@ if (!process.env.MONGO_URI) {
 }
 
 const uri = process.env.MONGO_URI;
+// options for the mongodb client
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -17,6 +18,7 @@ let client;
 let clientPromise;
 
 if (process.env.NODE_ENV === "development") {
+  // if we're in dev keep the connection to the db alive and dont reconnect on every refresh
   let globalWithMongo = global;
   globalWithMongo._mongoClientPromise = undefined;
 
@@ -26,6 +28,7 @@ if (process.env.NODE_ENV === "development") {
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
+  // if we're in prod then reconnect to the db everytime
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
