ecb287cd721c5e95bff12974861740448939d59f
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-13 16:31:22 +0500

    changed formatting

--- app/page.js
+++ app/page.js
@@ -3,10 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href="/create" className="btn btn-primary fixed top-10 right-10">📦 Add a Product</Link>
+      <Link href="/create" className="btn btn-primary fixed top-10 right-10">
+        📦 Add a Product
+      </Link>
       <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-5xl font-bold">Products</h1>
-
+        <h1 className="text-5xl font-bold">Products</h1>
       </div>
     </>
   );
